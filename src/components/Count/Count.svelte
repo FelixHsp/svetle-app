b2793e32b3bf7dea224fd8737dc5140312b5a7f1
Felix <13115412392@163.com>
2022-07-27 21:28:00 +0800

    feat: add Count

---
+++ src/components/Count/Count.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	let count: number = 0;
+	let numbers: number[] = [1, 2, 3, 4];
+
+	$: doubled = count * 2;
+
+	$: console.log(`the count is ${count}`);
+
+	$: sum = numbers.reduce((pre, cur) => pre + cur, 0);
+
+	const handleClick = () => {
+		count += 1;
+	};
+
+	const addNumber = () => {
+		numbers = [...numbers, numbers.length + 1];
+	};
+</script>
+
+<button on:click={handleClick}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+<p>
+	{count} doubled is {doubled}
+</p>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+<button on:click={addNumber}>Add a number</button>
